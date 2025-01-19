647e70175f8513ab21925a1c4199116494afae7f
vishal <vishalmahesh.study@gmail.com>
2025-01-20 01:03:55 +0530

    add footer

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/footer";
 import Header from "@/components/header";
 import React from "react";
 
@@ -6,6 +7,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
     <main className="">
       <Header />
       <div>{children}</div>
+      <Footer />
     </main>
   );
 };
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import Section from "./section";
+import Image from "next/image";
+import Link from "next/link";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="bg-secondary">
+      <Section className="py-10 flex items-center justify-between">
+        <div>
+          <Image src="/svg/logo-full.svg" alt="logo" width={100} height={100} />
+        </div>
+        <nav>
+            <ul className="flex items-center [&_li]:text-xs [&_li]:font-light gap-20">
+                <li><Link  href="/about">About</Link></li>
+                <li><Link  href="/privacypolicy">Privacy Policy</Link></li>
+                <li><Link  href="/contact">Contact Us</Link></li>
+            </ul>
+        </nav>
+      </Section>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
